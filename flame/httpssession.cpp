2d02644459dc5722689757a19273a85423214e67
Jeroen Wijenbergh <jeroenwijenbergh@protonmail.com>
2020-01-27 12:29:17 +0100

    Simplify stream and session data structures

--- flame/httpssession.cpp
+++ flame/httpssession.cpp
@@ -19,12 +19,12 @@ static ssize_t gnutls_push_trampoline(gnutls_transport_ptr_t h, const void *buf,
 
 // TODO: Remove duplicate code between TLSSession and this class
 HTTPSSession::HTTPSSession(std::shared_ptr<uvw::TcpHandle> handle,
-                            TCPSession::malformed_data_cb malformed_data_handler,
-                            TCPSession::got_dns_msg_cb got_dns_msg_handler,
-                            TCPSession::connection_ready_cb connection_ready_handler,
-                            handshake_error_cb handshake_error_handler, 
-                            Target target,
-                            HTTPMethod method)
+			   TCPSession::malformed_data_cb malformed_data_handler,
+			   TCPSession::got_dns_msg_cb got_dns_msg_handler,
+			   TCPSession::connection_ready_cb connection_ready_handler,
+			   handshake_error_cb handshake_error_handler, 
+			   Target target,
+			   HTTPMethod method)
     : TCPSession(handle, malformed_data_handler, got_dns_msg_handler, connection_ready_handler),
       _malformed_data{malformed_data_handler}, _got_dns_msg{got_dns_msg_handler}, _handle{handle}, _tls_state{LinkState::HANDSHAKE}, _handshake_error{handshake_error_handler}, _target{target}, _method{method}
 {
@@ -36,68 +36,35 @@ HTTPSSession::~HTTPSSession()
     gnutls_deinit(_gnutls_session);
 }
 
-void HTTPSSession::add_stream(http2_stream_data *stream_data)
-{
-    stream_data->next = _current_session->root.next;
-    _current_session->root.next = stream_data;
-    stream_data->prev = &_current_session->root;
-    if (stream_data->next) {
-    	stream_data->next->prev = stream_data;
-    }
-}
-
 http2_stream_data* HTTPSSession::create_http2_stream_data(std::unique_ptr<char[]> data, size_t len)
 {
     std::string uri = _target.uri;
     struct http_parser_url *u = _target.parsed;
+    std::string scheme(&uri[u->field_data[UF_SCHEMA].off], u->field_data[UF_SCHEMA].len);
     std::string authority(&uri[u->field_data[UF_HOST].off], u->field_data[UF_HOST].len);
     int32_t stream_id = -1;
-	//TODO: even though the RFC specifies dns-query?dns, this can depend on the implementation. Do not hardcode.
+    //TODO: even though the RFC specifies dns-query?dns, this can depend on the implementation. Do not hardcode.
     std::string path = "/dns-query";
-	if(_method == HTTPMethod::GET) {
-		path.append("?dns=");
-    	path.append(data.get(), len);
-	}
-    http2_stream_data *prev = {};
-    http2_stream_data *next = {};
-	std::string streamData(data.get(), len);
-    http2_stream_data *root = new http2_stream_data(prev, next, uri, u, authority, path, stream_id, streamData);
-    add_stream(root);
+    if(_method == HTTPMethod::GET) {
+	path.append("?dns=");
+	path.append(data.get(), len);
+    }
+    std::string streamData(data.get(), len);
+    http2_stream_data *root = new http2_stream_data(scheme, authority, path, stream_id, streamData);
     return root;
 }
 
-void HTTPSSession::initiate_session_data()
-{
-    std::string uri = _target.uri;
-    struct http_parser_url *u = _target.parsed;
-    std::string authority(&uri[u->field_data[UF_HOST].off], u->field_data[UF_HOST].len);
-    int32_t stream_id = -1;
+#define MAKE_NV(NAME, VALUE, VALUELEN)					\
+    {									\
+	(uint8_t *)NAME, (uint8_t *)VALUE, sizeof(NAME) - 1, VALUELEN,	\
+	    NGHTTP2_NV_FLAG_NONE					\
+	    }
 
-    std::string path = "/";
-    nghttp2_session *session = {};
-    http2_stream_data *prev = {};
-    http2_stream_data *next = {};
-	std::string data = "";
-    http2_stream_data stream_data = {prev, next, uri, u, authority, path, stream_id, data};
-    _current_session = new http2_session_data(session, stream_data);
-}
-
-http2_session_data* HTTPSSession::getSession()
-{
-    return this->_current_session;
-}
-
-#define MAKE_NV(NAME, VALUE, VALUELEN)                                         \
-  {                                                                            \
-    (uint8_t *)NAME, (uint8_t *)VALUE, sizeof(NAME) - 1, VALUELEN,             \
-        NGHTTP2_NV_FLAG_NONE                                                   \
-  }
-
-#define MAKE_NV2(NAME, VALUE)                                                  \
-  {                                                                            \
-    (uint8_t *)NAME, (uint8_t *)VALUE, sizeof(NAME) - 1, sizeof(VALUE)-1,    \
-        NGHTTP2_NV_FLAG_NONE                                                   \
-}
+#define MAKE_NV2(NAME, VALUE)						\
+    {									\
+	(uint8_t *)NAME, (uint8_t *)VALUE, sizeof(NAME) - 1, sizeof(VALUE)-1, \
+	    NGHTTP2_NV_FLAG_NONE					\
+	    }
 
 #define ARRLEN(x) (sizeof(x) / sizeof(x[0]))
 
@@ -111,19 +78,19 @@ static ssize_t send_callback(nghttp2_session *session, const uint8_t *data, size
 }
 
 void HTTPSSession::destroy_session()
- {
+{
     gnutls_certificate_free_credentials(_gnutls_cert_credentials);
     gnutls_deinit(_gnutls_session);
-    nghttp2_session_del(_current_session->session);
+    nghttp2_session_del(_current_session);
 }
 
 void HTTPSSession::process_receive(const uint8_t *data, size_t len) {
     const size_t MIN_DNS_QUERY_SIZE = 17;
     const size_t MAX_DNS_QUERY_SIZE = 512;
     if(len < MIN_DNS_QUERY_SIZE || len > MAX_DNS_QUERY_SIZE) {
-        std::cerr << "malformed data" << std::endl;
-        _malformed_data();
-        return;
+	std::cerr << "malformed data" << std::endl;
+	_malformed_data();
+	return;
     }
     auto buf = std::make_unique<char []>(len);
     memcpy(buf.get(), (const char*)data, len);
@@ -131,14 +98,14 @@ void HTTPSSession::process_receive(const uint8_t *data, size_t len) {
 }
 
 static int on_data_chunk_recv_callback(nghttp2_session *session, uint8_t flags,
-    int32_t stream_id, const uint8_t *data,
-    size_t len, void *user_data)
+				       int32_t stream_id, const uint8_t *data,
+				       size_t len, void *user_data)
 {
     HTTPSSession *class_session = (HTTPSSession *)user_data;
     auto req = nghttp2_session_get_stream_user_data(session, stream_id);
     if (!req) {
-        std::cout << "no stream data, on data chunk" << std::endl;
-        return 0;
+	std::cout << "no stream data, on data chunk" << std::endl;
+	return 0;
     }
     class_session->process_receive(data, len);
     return 0;
@@ -148,8 +115,8 @@ static int on_stream_close_callback(nghttp2_session *session, int32_t stream_id,
 {
     http2_stream_data *stream_data = static_cast<http2_stream_data *>(nghttp2_session_get_stream_user_data(session, stream_id));
     if (!stream_data) {
-        std::cout << "no stream data, stream close" << std::endl;
-        return 0;
+	std::cout << "no stream data, stream close" << std::endl;
+	return 0;
     }
     nghttp2_session_terminate_session(session, NGHTTP2_NO_ERROR);
     return 0;
@@ -162,7 +129,7 @@ void HTTPSSession::init_nghttp2()
     nghttp2_session_callbacks_set_send_callback(callbacks, send_callback);
     nghttp2_session_callbacks_set_on_data_chunk_recv_callback(callbacks, on_data_chunk_recv_callback);
     nghttp2_session_callbacks_set_on_stream_close_callback(callbacks, on_stream_close_callback);
-    nghttp2_session_client_new(&_current_session->session, callbacks, this);
+    nghttp2_session_client_new(&_current_session, callbacks, this);
     nghttp2_session_callbacks_del(callbacks);
 }
 
@@ -219,10 +186,10 @@ bool HTTPSSession::setup()
 
 void HTTPSSession::send_settings()
 {
-	//TODO: Find out why increasing this value still results in a maximum of 100 concurrent streams...
+    //TODO: Find out why increasing this value still results in a maximum of 100 concurrent streams...
     nghttp2_settings_entry settings[1] = { {NGHTTP2_SETTINGS_MAX_CONCURRENT_STREAMS, 100} };
     int val;
-    val = nghttp2_submit_settings(_current_session->session, NGHTTP2_FLAG_NONE, settings, ARRLEN(settings));
+    val = nghttp2_submit_settings(_current_session, NGHTTP2_FLAG_NONE, settings, ARRLEN(settings));
     if (val != 0) {
         std::cout << "Could not submit SETTINGS: " << nghttp2_strerror(val) << std::endl;
     }
@@ -230,7 +197,7 @@ void HTTPSSession::send_settings()
 
 void HTTPSSession::receive_response(const char data[], size_t len)
 {
-    ssize_t stream_id = nghttp2_session_mem_recv(_current_session->session, (const uint8_t *) data, len);
+    ssize_t stream_id = nghttp2_session_mem_recv(_current_session, (const uint8_t *) data, len);
     if (stream_id < 0) {
         std::cout << "Could not get HTTP request: " << nghttp2_strerror(stream_id);
         close();
@@ -241,7 +208,7 @@ void HTTPSSession::receive_response(const char data[], size_t len)
 int HTTPSSession::session_send()
 {
     int rv;
-    rv = nghttp2_session_send(_current_session->session);
+    rv = nghttp2_session_send(_current_session);
     if (rv != 0) {
         std::cout << "Fatal error: " << nghttp2_strerror(rv);
         return -1;
@@ -251,7 +218,7 @@ int HTTPSSession::session_send()
 
 void HTTPSSession::on_connect_event()
 {
-	initiate_session_data();
+    _current_session = {};
     do_handshake();
 }
 
@@ -263,86 +230,82 @@ void HTTPSSession::close()
 }
 
 static ssize_t post_data(nghttp2_session *session, int32_t stream_id, uint8_t *buf, size_t length, uint32_t *data_flags, nghttp2_data_source *source, void *user_data) {
-	auto stream_data = static_cast<http2_stream_data *>(nghttp2_session_get_stream_user_data(session, stream_id));
-	size_t nread = std::min(stream_data->data.size(), length);
-	memcpy(buf, stream_data->data.c_str(), nread);
-	*data_flags = NGHTTP2_DATA_FLAG_EOF;
-	return nread;
+    auto stream_data = static_cast<http2_stream_data *>(nghttp2_session_get_stream_user_data(session, stream_id));
+    size_t nread = std::min(stream_data->data.size(), length);
+    memcpy(buf, stream_data->data.c_str(), nread);
+    *data_flags = NGHTTP2_DATA_FLAG_EOF;
+    return nread;
 }
 
 void HTTPSSession::write(std::unique_ptr<char[]> data, size_t len)
 {
     int32_t stream_id;
     http2_stream_data *stream_data = create_http2_stream_data(std::move(data), len);
-    std::string uri = stream_data->uri;
-    const struct http_parser_url *u = stream_data->u;
-    std::string current_path = stream_data->path;
-	//TODO: Duplicate code
-	if(_method == HTTPMethod::GET) {
-		nghttp2_nv hdrs[] = {
-		  MAKE_NV2(":method", "GET"),
-		  MAKE_NV(":scheme", &uri[u->field_data[UF_SCHEMA].off], u->field_data[UF_SCHEMA].len),
-		  MAKE_NV(":authority", stream_data->authority.c_str(), stream_data->authority.size()),
-		  MAKE_NV(":path", current_path.c_str(), current_path.size()),
-		  MAKE_NV2("accept", "application/dns-message"),
-		};
-		stream_id = nghttp2_submit_request(_current_session->session, NULL, hdrs, ARRLEN(hdrs), NULL, stream_data);
-	} else {
-		nghttp2_nv hdrs[] = {
-		  MAKE_NV2(":method", "POST"),
-		  MAKE_NV(":scheme", &uri[u->field_data[UF_SCHEMA].off],
-				  u->field_data[UF_SCHEMA].len),
-		  MAKE_NV(":authority", stream_data->authority.c_str(), stream_data->authority.size()),
-		  MAKE_NV(":path", current_path.c_str(), current_path.size()),
-		  MAKE_NV2("accept", "application/dns-message"),
-		  MAKE_NV2("content-type", "application/dns-message"),
-		  MAKE_NV("content-length", std::to_string(len).c_str(), std::to_string(len).size())
-		};
-		nghttp2_data_provider provider;
-		provider.read_callback = post_data;
-		stream_id = nghttp2_submit_request(_current_session->session, NULL, hdrs, ARRLEN(hdrs), &provider, stream_data);
-	}
+    //TODO: Duplicate code
+    if(_method == HTTPMethod::GET) {
+        nghttp2_nv hdrs[] = {
+                             MAKE_NV2(":method", "GET"),
+                             MAKE_NV(":scheme", stream_data->scheme.c_str(), stream_data->scheme.size()),
+                             MAKE_NV(":authority", stream_data->authority.c_str(), stream_data->authority.size()),
+                             MAKE_NV(":path", stream_data->path.c_str(), stream_data->path.size()),
+                             MAKE_NV2("accept", "application/dns-message"),
+        };
+        stream_id = nghttp2_submit_request(_current_session, NULL, hdrs, ARRLEN(hdrs), NULL, stream_data);
+    } else {
+        nghttp2_nv hdrs[] = {
+                             MAKE_NV2(":method", "POST"),
+                             MAKE_NV(":scheme", stream_data->scheme.c_str(), stream_data->scheme.size()),
+                             MAKE_NV(":authority", stream_data->authority.c_str(), stream_data->authority.size()),
+                             MAKE_NV(":path", stream_data->path.c_str(), stream_data->path.size()),
+                             MAKE_NV2("accept", "application/dns-message"),
+                             MAKE_NV2("content-type", "application/dns-message"),
+                             MAKE_NV("content-length", std::to_string(len).c_str(), std::to_string(len).size())
+        };
+        nghttp2_data_provider provider;
+        provider.read_callback = post_data;
+        stream_id = nghttp2_submit_request(_current_session, NULL, hdrs, ARRLEN(hdrs), &provider, stream_data);
+    }
     if (stream_id < 0) {
         std::cout << "Could not submit HTTP request: " << nghttp2_strerror(stream_id);
     }
 
     stream_data->stream_id = stream_id;
 
-	if(session_send() != 0) {
-       std::cerr << "failed to send" << std::endl;
-	}
+    if(session_send() != 0) {
+        std::cerr << "failed to send" << std::endl;
+    }
 }
 
 void HTTPSSession::receive_data(const char data[], size_t _len)
 {
     _pull_buffer.append(data, _len);
-	switch(_tls_state) {
-		case LinkState::HANDSHAKE:
-			do_handshake();
-			break;
-		case LinkState::DATA: 
-			char buf[2048];
-			for (;;) {
-				ssize_t len = gnutls_record_recv(_gnutls_session, buf, sizeof(buf));
-				if (len > 0) {
-					receive_response(buf, len);
-				} else {
-					if(len == GNUTLS_E_AGAIN) {
-                        // Check if we don't have any data left to read
-						if(_pull_buffer.empty()) {
-							break;
-						}
-						continue;
-					} else if(len == GNUTLS_E_INTERRUPTED) {
-						continue;
-					}
-					break;
-				}
-			}
-			break;
-		case LinkState::CLOSE:
-			break;
-	}
+    switch(_tls_state) {
+    case LinkState::HANDSHAKE:
+        do_handshake();
+        break;
+    case LinkState::DATA: 
+        char buf[2048];
+        for (;;) {
+            ssize_t len = gnutls_record_recv(_gnutls_session, buf, sizeof(buf));
+            if (len > 0) {
+                receive_response(buf, len);
+            } else {
+                if(len == GNUTLS_E_AGAIN) {
+                    // Check if we don't have any data left to read
+                    if(_pull_buffer.empty()) {
+                        break;
+                    }
+                    continue;
+                } else if(len == GNUTLS_E_INTERRUPTED) {
+                    continue;
+                }
+                break;
+            }
+        }
+        break;
+    case LinkState::CLOSE:
+        break;
+    }
 }
 
 void HTTPSSession::send_tls(void* data, size_t len)
--- flame/httpssession.h
+++ flame/httpssession.h
@@ -10,23 +10,15 @@
 #include "target.h"
 
 struct http2_stream_data {
-    http2_stream_data(http2_stream_data *_prev, http2_stream_data *_next, std::string _uri, struct http_parser_url *_u, std::string _authority, std::string _path, int32_t _stream_id, std::string _data):
-    prev(_prev), next(_next), uri(_uri), u(_u), authority(_authority), path(_path), stream_id(_stream_id), data(_data) {}
-    struct http2_stream_data *prev, *next;
-    std::string uri;
-    struct http_parser_url *u;
+    http2_stream_data(std::string _scheme, std::string _authority, std::string _path, int32_t _stream_id, std::string _data):
+	scheme(_scheme), authority(_authority), path(_path), stream_id(_stream_id), data(_data) {}
+    std::string scheme;
     std::string authority;
     std::string path;
     int32_t stream_id;
     int32_t id;
-	std::string data;
+    std::string data;
 }; 
-
-struct http2_session_data {
-    http2_session_data(nghttp2_session *_session, http2_stream_data _root): session(_session), root(_root) {}
-    nghttp2_session *session;
-    http2_stream_data root;
-};
 
 class HTTPSSession : public TCPSession
 {
@@ -35,12 +27,12 @@ public:
     using handshake_error_cb =  std::function<void()>;
 
     HTTPSSession(std::shared_ptr<uvw::TcpHandle> handle,
-                TCPSession::malformed_data_cb malformed_data_handler,
-                TCPSession::got_dns_msg_cb got_dns_msg_handler,
-                TCPSession::connection_ready_cb connection_ready_handler,
-                handshake_error_cb handshake_error_handler,
-                Target target,
-                HTTPMethod method);
+		 TCPSession::malformed_data_cb malformed_data_handler,
+		 TCPSession::got_dns_msg_cb got_dns_msg_handler,
+		 TCPSession::connection_ready_cb connection_ready_handler,
+		 handshake_error_cb handshake_error_handler,
+		 Target target,
+		 HTTPMethod method);
     virtual ~HTTPSSession();
 
     virtual bool setup();
@@ -48,7 +40,6 @@ public:
     virtual void on_connect_event();
 
     void send_tls(void* data, size_t len);
-    http2_session_data* getSession();
     void init_nghttp2();
     void send_settings();
     void receive_response(const char data[], size_t len);
@@ -71,7 +62,6 @@ protected:
     void destroy_stream();
     void destroy_session();
     void do_handshake();
-    void initiate_session_data();
 
 private:
     malformed_data_cb _malformed_data;
@@ -82,7 +72,7 @@ private:
     Target _target;
     HTTPMethod _method;
 
-    http2_session_data* _current_session;
+    nghttp2_session* _current_session;
     std::string _pull_buffer;
 
     gnutls_session_t _gnutls_session;
