68c3b936a9d03488727cf8f38cf63d12a046cbab
Jan Včelák <jv@fcelda.cz>
2025-12-10 15:46:12 +0100

    3rd: update dockopt.cpp to the latest version

---
+++ 3rd/update_docopt.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -xe
+
+SRC_REPO=${SRC_REPO:-https://github.com/docopt/docopt.cpp}
+DESTDIR=${DESTDDIR:-$(dirname "$0")/docopt.cpp}
+REVISION=${REVISION:-master}
+
+SRCDIR=$(mktemp -d)
+# shellcheck disable=SC2064
+trap "rm -rf \"$SRCDIR\"" SIGINT SIGTERM EXIT
+
+git clone --depth=1 --branch="$REVISION" -- "$SRC_REPO" "$SRCDIR"
+
+rm -rf "$DESTDIR"
+install -m 0755 -d "$DESTDIR"
+install -m 0644 "$SRCDIR"/LICENSE* "$DESTDIR"
+install -m 0644 "$SRCDIR"/docopt*.{h,cpp} "$DESTDIR"
+
+git --git-dir "$SRCDIR/.git" --work-tree "$SRCDIR" rev-parse HEAD | tee "$DESTDIR/VERSION"
