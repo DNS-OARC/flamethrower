703bbd99aacba181b2c9e75eea4e5c690e94fcae
Jan Včelák <jv@fcelda.cz>
2025-12-11 17:33:31 +0100

    fix docopt.cpp usage

--- 3rd/update_docopt.sh
+++ 3rd/update_docopt.sh
@@ -14,7 +14,9 @@ git clone --depth=1 --branch="$REVISION" -- "$SRC_REPO" "$SRCDIR"
 
 rm -rf "$DESTDIR"
 install -m 0755 -d "$DESTDIR"
+install -m 0755 -d "$DESTDIR/"{src,include}
 install -m 0644 "$SRCDIR"/LICENSE* "$DESTDIR"
-install -m 0644 "$SRCDIR"/docopt*.{h,cpp} "$DESTDIR"
+install -m 0644 "$SRCDIR"/docopt.cpp "$DESTDIR/src"
+install -m 0644 "$SRCDIR"/docopt*.h "$DESTDIR/include"
 
 git --git-dir "$SRCDIR/.git" --work-tree "$SRCDIR" rev-parse HEAD | tee "$DESTDIR/VERSION"
